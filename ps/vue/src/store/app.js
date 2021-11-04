9c0ac4c2fb5cfac22e191561785223935a53b761
dstreppa <Lasolita1976>
2021-11-04 11:18:18 +0100

    Deleted vue folder

--- ps/vue/src/store/app.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  types: [
-    // this line is used by starport scaffolding
-		{ type: "meterAccount", fields: ["meter", "account", "admin", ] },
-		{ type: "parameters", fields: ["prodConvFactor", "consConvFactor", "maxConsumption", "penalty", ] },
-  ],
-};
